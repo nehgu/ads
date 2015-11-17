9fe97009d65d9857b19386daba4b441449b60ca9
nehgu <nehu36@yahoo.com>
2015-11-17 03:03:53 -0500

    adding scapper to search img urls

--- MyTool/get_img.py
+++ MyTool/get_img.py
@@ -2,7 +2,7 @@
     Crawler_Tool
     ~~~~~~~~~~~~
 
-    A module that extracts all div objects from a website url.
+    A module that extracts all image objects from a website url.
 
 """
 
@@ -10,20 +10,17 @@ import sys
 from bs4 import BeautifulSoup
 import urllib
 import urlparse
-from bs4 import UnicodeDammit
 
 """ Store the html page in soup object """
 url='http://www.columbia.edu'
 r = urllib.urlopen(url).read()
 soup = BeautifulSoup(r, "html.parser")
-"Remove all script elements"
-for script in soup(["script"]):
-    script.extract() 
 
 """ Generate all embedded text in a text file all_text.txt    """
-texts = soup.findAll(text=True)
-f = open('all_text.txt', 'w')
-f.write(str(texts))
+f = open('all_img.txt', 'w')
+for tag in soup.findAll('img', src=True):
+    image_url = urlparse.urljoin(url, tag['src'])
+    f.write(image_url+'\n')
 f.close()
 
 
